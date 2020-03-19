2b9ce17139f8f581c8d63d09ca57195bd442343f
SofiaCantero24 <canterosofia24@gmail.com>
2020-03-18 22:52:36 -0300

    XCode project created

---
+++ Pods/Target Support Files/Pods-CoronavirusApp-CoronavirusAppUITests/Pods-CoronavirusApp-CoronavirusAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoronavirusApp_CoronavirusAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CoronavirusApp_CoronavirusAppUITests
+@end
---
+++ Pods/Target Support Files/Pods-CoronavirusAppTests/Pods-CoronavirusAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoronavirusAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_CoronavirusAppTests
+@end
