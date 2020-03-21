336478fa1bec7d1d540bf81441fb92f38bae36f1
SofiaCantero24 <canterosofia24@gmail.com>
2020-03-21 09:56:03 -0300

    Pods deleted

--- Pods/Target Support Files/Pods-CoronavirusApp/Pods-CoronavirusApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CoronavirusApp : NSObject
-@end
-@implementation PodsDummy_Pods_CoronavirusApp
-@end
--- Pods/Target Support Files/Pods-CoronavirusAppTests/Pods-CoronavirusAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CoronavirusAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_CoronavirusAppTests
-@end
